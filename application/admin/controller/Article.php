f172b46b0eb5411fdc81d8b4dd41f04e7d8c3612
xiayulei <xiayulei@gmail.com>
2016-11-28 13:46:45 +0800

    保存、更新文章增加验证器验证

--- application/admin/controller/Article.php
+++ application/admin/controller/Article.php
@@ -59,12 +59,17 @@ class Article extends AdminBase {
      */
     public function save() {
         if ($this->request->isPost()) {
-            $data = $this->request->post();
+            $data            = $this->request->post();
+            $validate_result = $this->validate($data, 'Article');
 
-            if ($this->article_model->allowField(true)->save($data)) {
-                $this->success('保存成功');
+            if ($validate_result !== true) {
+                $this->error($validate_result);
             } else {
-                $this->error('保存失败');
+                if ($this->article_model->allowField(true)->save($data)) {
+                    $this->success('保存成功');
+                } else {
+                    $this->error('保存失败');
+                }
             }
         }
     }
@@ -86,12 +91,17 @@ class Article extends AdminBase {
      */
     public function update($id) {
         if ($this->request->isPost()) {
-            $data = $this->request->post();
+            $data            = $this->request->post();
+            $validate_result = $this->validate($data, 'Article');
 
-            if ($this->article_model->allowField(true)->save($data, $id) !== false) {
-                $this->success('更新成功');
+            if ($validate_result !== true) {
+                $this->error($validate_result);
             } else {
-                $this->error('更新失败');
+                if ($this->article_model->allowField(true)->save($data, $id) !== false) {
+                    $this->success('更新成功');
+                } else {
+                    $this->error('更新失败');
+                }
             }
         }
     }
