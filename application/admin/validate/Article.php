269a51dd9c71bc8e2b8f29dedcd9020c6dfa2310
xiayulei <xiayulei@gmail.com>
2016-11-28 14:00:00 +0800

    补充文章验证器栏目选择验证规则

--- application/admin/validate/Article.php
+++ application/admin/validate/Article.php
@@ -5,11 +5,13 @@ use think\Validate;
 
 class Article extends Validate {
     protected $rule = [
+        'cid'   => 'require',
         'title' => 'require',
         'sort'  => 'require|number'
     ];
 
     protected $message = [
+        'cid.require'   => '请选择所属栏目',
         'title.require' => '请输入标题',
         'sort.require'  => '请输入排序',
         'sort.number'   => '排序只能填写数字'
