a56ff50f60e785268a6ca80b4f4ed9953cdc9a8e
WuAng <wuangv@gmail.com>
2017-07-13 18:49:52 +0800

    添加生成model command

---
+++ application/admin/model/User.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\admin\model;
+
+use think\Model;
+
+/**
+ * This is the model class for table "os_user".
+ *
+ * @property string $id
+ * @property string $username
+ * @property string $password
+ * @property string $mobile
+ * @property string $email
+ * @property integer $status
+ * @property string $create_time
+ * @property string $last_login_time
+ * @property string $last_login_ip
+ */
+class User extends Model
+{
+
+}
---
+++ application/model/User.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\model;
+
+use think\Model;
+
+/**
+* This is the model class for table "os_user".
+*
+* @property string $id
+* @property string $username
+* @property string $password
+* @property string $mobile
+* @property string $email
+* @property integer $status
+* @property string $create_time
+* @property string $last_login_time
+* @property string $last_login_ip
+
+*/
+class User extends Model
+{
+
+}
